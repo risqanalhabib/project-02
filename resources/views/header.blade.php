1459512f0e0311893bfa971036d5977874e7356d
khairatulnisa <khairatulnisa616@gmail.com>
2024-11-06 05:14:33 +0000

    rubah

---
+++ resources/views/header.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Bersama</title>
+
+    <link rel="stylesheet" href="css_berita/header.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body class="bodyhd">
+
+    <div class="containerhd mt-5">
+        <div class="cardhd p-4">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="foto/h.jpeg" class="imgh img-fluid rounded-start" alt="Welcome Image">
+                </div>
+                <div class="col-md-6">
+                    <h3 class="card-title">SELAMAT DATANG DI WEB KAMI</h3>
+                    <p class="text-muted">"Hasil kerja kami berdua"</p>
+                    <p>Di sini Anda dapat menemukan informasi mengenai anggota kami, dan beberapa berita yang telah kami buat. Anda juga dapat menambahkan berita di sini.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
